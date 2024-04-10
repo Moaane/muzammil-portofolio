c36f5a8d467102bd071224dc060d050966dc46a1
Moaane <gpro0150@gmail.com>
2024-04-11 06:34:24 +0700

    fdsf

--- src/layout/DefaultLayout.jsx
+++ src/layout/DefaultLayout.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function DefaultLayout({ children }) {
   return (
-    <div className="w-screen flex flex-col items-center px-4 md:px-8 lg:px-24 gap-24 lg:gap-32 py-4 mt-12 md:mt-8 lg:mt-12">
+    <div className="w-screen flex flex-col items-center px-4 md:px-8 lg:px-24 gap-10 md:gap-20 lg:gap-32 py-4 mt-12 md:mt-8 lg:mt-12">
       {children}
     </div>
   );
