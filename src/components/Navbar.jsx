4a7b789597da74b39d876d8c60b91f5e7bfb0cb3
Moaane <gpro0150@gmail.com>
2024-04-11 07:35:56 +0700

    asd

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ export default function Navbar() {
   
   
   return (
-    <div className="fixed w-screen h-[5vh] md:h-[8vh] lg:h-[10vh] flex justify-center">
+    <div className="fixed w-screen h-[5vh] md:h-[8vh] lg:h-[10vh] flex justify-center bg-white">
       
       {/* Mobile Navbar */}
       <div className="navbar-center w-screen h-full flex lg:hidden items-center justify-between px-4 md:px-8">
